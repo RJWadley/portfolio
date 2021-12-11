4de386bd30079a6cd60872f16e9712b930ccb76c
RJWadley <robbiethewadley@gmail.com>
2021-12-11 00:25:40 -0700

    inital design

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ *  SIKE you thought
+ *
+ *  I wanted to challenge myself a bit,
+ *  so I built this site without any JS
+ *
+ *  Learn more about the awesome
+ *  CSS techniques I use here:
+ *
+ *  http://bitly.com/98K8eH
+ */
